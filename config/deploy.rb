c33842b6de3236a3794926f97b80b3b0d8a2329a
🐉 <ian@secretnodes.org>
2020-04-09 14:03:59 -0700

    Update deploy.rb
    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.11.0'
+lock '~> 3.13.0'
 
 set :application, 'puzzle'
 set :repo_url, 'git@github.com:secretnodes/puzzle.git'
