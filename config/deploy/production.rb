278ddf5e85733a0610f580f9d05ce371984e3c3a
jacobtjackson <jacob.t.jackson@gmail.com>
2020-05-03 13:59:24 -0700

    try to fix capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server ENV['DEPLOY_HOST'], user: 'root', roles: %w{ app db web }
 set :ssh_options, {
-  keys: ENV['DEPLOY_KEYS'].split(','),
+  keys: ['~/.ssh/puzzle_id_rsa2'],
   forward_agent: true
 }
 
--- setup/templates/deploy.sh
+++ setup/templates/deploy.sh
@@ -4,4 +4,8 @@ export RAILS_ENV={{RAILS_ENV}}
 export DEPLOY_USER={{REMOTE_USER}}
 export DEPLOY_HOST={{HOST}}
 export DEPLOY_KEYS={{KEY}}
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+ eval `ssh-agent -s`
+ ssh-add ~/.ssh/puzzle_id_rsa
+fi
 bin/bundle exec cap $RAILS_ENV deploy
