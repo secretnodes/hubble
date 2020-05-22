062dce47168bdad2946cd08838fadd5d735935bc
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-05-22 14:41:00 -0700

    add privacy policy and pop up at the bottom

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,8 @@ class HomeController < ApplicationController
   def catch_404
     raise ActionController::RoutingError.new(params[:path])
   end
+
+  def privacy_policy
+    page_title 'Puzzle Privacy Policy'
+  end
 end
