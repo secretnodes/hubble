6830f79bdb82c35a834b9c4b3819abd6ffcec16c
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-06-13 18:40:03 -0700

    update user for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server ENV['DEPLOY_HOST'], user: 'root', roles: %w{ app db web }
+server ENV['DEPLOY_HOST'], user: 'i7-nuc10', roles: %w{ app db web }
 set :ssh_options, {
   keys: ENV['DEPLOY_KEYS'].split(','),
   forward_agent: true
