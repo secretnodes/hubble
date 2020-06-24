05d8ab85a320975890e4f9c011b7b57fa25b590b
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-06-23 19:49:39 -0700

    switch to modules

--- app/controllers/API/v1/base_controller.rb
+++ app/controllers/API/v1/base_controller.rb
@@ -1,6 +1,10 @@
-class API::V1::BaseController < ApplicationController
-  skip_before_action :http_basic_auth
-  skip_before_action :get_user
+module Api
+  module V1
+    class BaseController < ApplicationController
+      skip_before_action :http_basic_auth
+      skip_before_action :get_user
 
-  respond_to :json
+      respond_to :json
+    end
+  end
 end
