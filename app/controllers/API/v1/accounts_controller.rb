8e193000c832145d39ada4284fc3f576bf784cde
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-06-23 19:52:12 -0700

    switch to lowercase api

--- app/controllers/API/v1/accounts_controller.rb
+++ app/controllers/API/v1/accounts_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::AccountsController < ApplicationController
+class Api::V1::AccountsController < ApplicationController
   def balance
     chain = Secret::Chain.find params[:chain_id]
 
