9b8aa93eb17dae142f687b4cde77c752e5d079e7
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-06-23 19:55:03 -0700

    starting to get old

--- app/controllers/API/v1/accounts_controller.rb
+++ app/controllers/API/v1/accounts_controller.rb
@@ -1,29 +1,33 @@
-class Api::V1::AccountsController < ApplicationController
-  def balance
-    chain = Secret::Chain.find params[:chain_id]
+module Api
+  module V1
+    class AccountsController < ApplicationController
+      def balance
+        chain = Secret::Chain.find params[:chain_id]
 
-    syncer = chain.syncer( 3_000 )
-    balance = syncer.get_account_balances params[:address]
+        syncer = chain.syncer( 3_000 )
+        balance = syncer.get_account_balances params[:address]
 
-    if balance
-      render json: { balance: balance }, status: :ok
-    else
-      render json: { error: "We could not return the balance for that address. Please try again." }, status: :internal_server_error
-    end
+        if balance
+          render json: { balance: balance }, status: :ok
+        else
+          render json: { error: "We could not return the balance for that address. Please try again." }, status: :internal_server_error
+        end
 
-    return
-  end
+        return
+      end
 
-  def info
-    chain = Secret::Chain.find params[:chain_id]
+      def info
+        chain = Secret::Chain.find params[:chain_id]
 
-    syncer = chain.syncer( 3_000 )
-    info = syncer.get_account_info params[:address]
+        syncer = chain.syncer( 3_000 )
+        info = syncer.get_account_info params[:address]
 
-    if info
-      render json: info['value'], status: :ok
-    else
-      render json: { error: "We could not return the info for that address. Please try again." }, status: :internal_server_error
+        if info
+          render json: info['value'], status: :ok
+        else
+          render json: { error: "We could not return the info for that address. Please try again." }, status: :internal_server_error
+        end
+      end
     end
   end
 end
