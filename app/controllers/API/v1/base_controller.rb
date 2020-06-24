79a4813f1332547906ba8079d02d1de3d15fd93a
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-06-23 19:57:44 -0700

    more api config

--- app/controllers/API/v1/base_controller.rb
+++ app/controllers/API/v1/base_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      skip_before_action :http_basic_auth
-      skip_before_action :get_user
+      # skip_before_action :http_basic_auth
+      # skip_before_action :get_user
 
       respond_to :json
     end
