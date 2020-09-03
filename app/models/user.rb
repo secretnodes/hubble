55f970a12a22afa1e6fcc71abce31fa6720e317e
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-09-03 14:48:20 -0700

    remove name from users and add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   MASQ_TIMEOUT = 10.minutes
 
