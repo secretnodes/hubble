acdd6271f4cd8dd133f54c4d030ee4b890052a2d
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-10-13 13:04:25 -0700

    ensure users have username when leaving comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,11 @@ class CommentsController < ApplicationController
   layout 'none'
 
   def create
+    user = User.find comment_params[:user_id]
+    if user.username.blank?
+      flash[:error] = "You must set a username in your #{view_context.link_to 'Account Settings', edit_user_registration_path} before commenting.".html_safe
+      redirect_back(fallback_location: root_path) and return
+    end
     @comment = Comment.new(comment_params)
 
     if @comment.save!
