5fc22cc82a169c6e9e4cf3f8d16c5ecffaafad55
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-10-13 12:54:40 -0700

    Basic comment functionality completed

--- app/controllers/common/governance/proposals_controller.rb
+++ app/controllers/common/governance/proposals_controller.rb
@@ -5,7 +5,8 @@ class Common::Governance::ProposalsController < Common::BaseController
     @proposal = @chain.governance_proposals.find_by(ext_id: params[:id])
     @tally_result = @chain.namespace::ProposalTallyDecorator.new(@proposal)
     
-    @comments = @proposal.comments
+    sort_direction = params[:comment_sort].present? ? params[:comment_sort].downcase.to_sym : :asc
+    @comments = @proposal.comments.unscope(:order).order(created_at: sort_direction)
 
     page_title @chain.network_name, @chain.name, @proposal.title
     meta_description @proposal.description.truncate(160, separator: '...')
--- app/webpacker/src/javascripts/page/common/governance-proposal.js
+++ app/webpacker/src/javascripts/page/common/governance-proposal.js
@@ -3,4 +3,18 @@ $(document).ready( function() {
 
   new App.Common.GovProposalDepositModal( $('#proposal-deposit-modal') )
   new App.Common.GovProposalVoteModal( $('#proposal-vote-modal') )
+
+  $('.collapse-comments-btn').click(function() {
+    $('.comments').toggleClass('d-none');
+    $(this).toggleClass('d-none');
+    $('.comment-sort-btn').toggleClass('d-none');
+    $('.show-comments-btn').toggleClass('d-none');
+  });
+
+  $('.show-comments-btn').click(function() {
+    $('.comments').toggleClass('d-none');
+    $(this).toggleClass('d-none');
+    $('.comment-sort-btn').toggleClass('d-none');
+    $('.collapse-comments-btn').toggleClass('d-none');
+  })
 } )
