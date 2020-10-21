0cac1fa4103e25992ec6b5fbd2f3ac4b8024e291
Jacob Jackson <jacob.t.jackson@gmail.com>
2020-10-21 14:10:29 -0700

    update gov picker and add foundation abilities

--- app/webpacker/src/javascripts/page/common/governance-index.js
+++ app/webpacker/src/javascripts/page/common/governance-index.js
@@ -1,6 +1,10 @@
 $(document).ready( function() {
   if( !_.includes(App.mode, 'governance-index') ) { return }
 
+  $('#gov-page-selector').change( (e) => {
+    window.location = $('#gov-page-selector :selected').val();
+  })
+
   new App.Common.GovProposalsTable( $('.gov-proposals-table') ).render()
   new App.Common.GovProposalSubmitModal( $('#proposal-modal') )
 } )
